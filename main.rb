abd26c021476f01927b87065ba5a37c1b46e2ede
Andrew <asbeane@gmail.com>
2016-01-28 20:21:46 -0500

    Small fix to main

--- main.rb
+++ main.rb
@@ -15,27 +15,30 @@ def main()
 	print_final = ARGV[2].to_i
 
 	# Take command Arg to indicate Number of Loops
+
+
+	pieces = Array.new
+	pieces.push(I.new())
+	pieces.push(O.new())
+	pieces.push(J.new())
+	pieces.push(T.new())
+	pieces.push(S.new())
+
+	randomVariable = Random.new
 	for i in 0...ARGV[0].to_i
-		x = Random.new.rand(5...30)
-		y = Random.new.rand(5...30)
+		x = randomVariable.rand(5...30)
+		y = randomVariable.rand(5...30)
 		# Initialize Board to random value between 1-30 by 1-30
 		testBoard = Board.new(x, y)
 
 		# Use Area of Board with average density of shape to get a shape count (pseudo-randomly)
-		density = Random.new.rand(1...( (x*y)/14))
-
+		density = randomVariable.rand(1...( (x*y)/14))
 
 		# Draw Initial Board
 		if print_init == 1
 			testBoard.draw_board()
 		end
 
-		pieces = Array.new
-		pieces.push(I.new())
-		pieces.push(O.new())
-		pieces.push(J.new())
-		pieces.push(T.new())
-		pieces.push(S.new())
 
 		score = 0
 		orientation = -1
@@ -43,9 +46,9 @@ def main()
 		# Adding random pieces to loops
 		i = 0
 		while i < density
-			temp = Random.new.rand(pieces.length)
+			temp = randomVariable.rand(pieces.length)
 			#temp = 3
-				if (orientation = pieces[temp].can_place(j = Random.new.rand(1...x),k = Random.new.rand(1...y), testBoard.instance_variable_get(:@board), Random.new.rand(8))) != -1
+				if (orientation = pieces[temp].can_place(j = randomVariable.rand(1...x),k = randomVariable.rand(1...y), testBoard.instance_variable_get(:@board), Random.new.rand(8))) != -1
 					#orientation = 2
 					pieces[temp].add_piece(j,k,testBoard.instance_variable_get(:@board), orientation)
 					i += 1
